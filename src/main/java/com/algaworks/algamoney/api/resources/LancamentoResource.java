845e175e972cd214ad6f95b7a840b6b78867bf3f
Eduardo Melo <eduardo.phellip@gmail.com>
2018-09-12 02:07:36 -0400

    adição da funcionanlidade de deletar lancamento

--- src/main/java/com/algaworks/algamoney/api/resources/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/api/resources/LancamentoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,5 +54,11 @@ public class LancamentoResource {
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.body(lancamentoSalvo);
+	}
+
+	@DeleteMapping("/{codigo}")
+	public ResponseEntity<Void> deletar(@PathVariable Long codigo) {
+		lancamentoService.deletarPorCodigo(codigo);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/algaworks/algamoney/api/services/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/services/LancamentoService.java
@@ -45,4 +45,9 @@ public class LancamentoService {
 		
 		return lancamentoRepository.save(lancamento);
 	}
+
+	public void deletarPorCodigo(Long codigo) {
+		codigo = Objects.requireNonNull(codigo, "Codigo do Lancamento é obrigatório.");
+		lancamentoRepository.delete(codigo);
+	}
 }
